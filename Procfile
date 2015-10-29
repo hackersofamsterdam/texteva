ee6b32bb86e9abb22d81711c02bd3e6446ae4802
zenry <henry@postb.us>
2015-10-29 12:49:05 +0100

    fc

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec ruby ./eva.rb
---
+++ eva.rb
@@ -0,0 +1,10 @@
+require 'telegrammer'
+
+bot = Telegrammer::Bot.new '156127274:AAGBC_Jiwgb5RXai7xaTuAF3if_6ZLUUB4U'
+
+bot.set_webhook('')
+
+bot.get_updates do |message|
+  puts "In chat #{message.chat.id}, @#{message.from.username} said: #{message.text}"
+  bot.send_message(chat_id: message.chat.id, text: "You said: #{message.text}")
+end
