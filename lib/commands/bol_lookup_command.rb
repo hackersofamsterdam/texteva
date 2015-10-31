3b312c9a62813b34f359cc2aa73dec324d452bb7
zenry <henry@postb.us>
2015-10-31 21:50:07 +0100

    better naming

--- lib/commands/bol_lookup_command.rb
+++ lib/commands/bol_lookup_command.rb
@@ -18,9 +18,9 @@ class BolLookupCommand < Command
   def invoke!
     response = BolClient.new.catalog match[:product]
     json = MultiJson.load response.body
-    store = Fridge.find_by_chat_id(message.chat.id) || Fridge.new(chat_id: message.chat.id)
+    fridge = Fridge.find_by_chat_id(message.chat.id) || Fridge.new(chat_id: message.chat.id)
 
-    store.update contents: json['products']
+    fridge.update contents: json['products']
 
     reply "I found a *#{json['products'][0]['title']}* in the Bol catalog.\n" \
           "Here it is: #{json['products'][0]['urls'][0]['value']}"
